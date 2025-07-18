ffc979fb39280236d5cc5d09b1251a4a7761743f
Holger Kohnen <h.kohnen@gmail.com>
2025-07-18 18:01:07 +0200

    add index

--- main.go
+++ main.go
@@ -34,8 +34,88 @@ func flattenMap(data map[string]interface{}, prefix string, out map[string]inter
 	}
 }
 
+// Index structure: map[propertyKey][propertyValue][]uuid
+var (
+	index     = make(map[string]map[string][]string)
+	blacklist = make(map[string]bool)
+	maxIndexValues = 10
+	maxLogs = 10000
+)
+
+func toString(val interface{}) string {
+	switch v := val.(type) {
+	case string:
+		return v
+	case fmt.Stringer:
+		return v.String()
+	case float64:
+		return fmt.Sprintf("%g", v)
+	case float32:
+		return fmt.Sprintf("%g", v)
+	case int, int8, int16, int32, int64:
+		return fmt.Sprintf("%d", v)
+	case uint, uint8, uint16, uint32, uint64:
+		return fmt.Sprintf("%d", v)
+	case bool:
+		return fmt.Sprintf("%t", v)
+	case nil:
+		return ""
+	default:
+		return fmt.Sprintf("%v", v)
+	}
+}
+
+func updateIndex(uuid string, flat map[string]interface{}) {
+	for k, v := range flat {
+		valStr := toString(v)
+		if valStr == "" {
+			continue // omit empty values
+		}
+		if blacklist[k] {
+			continue // skip blacklisted properties
+		}
+		if _, ok := index[k]; !ok {
+			index[k] = make(map[string][]string)
+		}
+		valMap := index[k]
+		valMap[valStr] = append(valMap[valStr], uuid)
+		// Blacklist if too many unique values
+		if len(valMap) > maxIndexValues {
+			delete(index, k)
+			blacklist[k] = true
+			// TODO: Notify websocket clients with drop_index
+		}
+	}
+}
+
+func removeFromIndex(uuid string, flat map[string]interface{}) {
+	for k, v := range flat {
+		valStr := toString(v)
+		if valMap, ok := index[k]; ok {
+			if uuids, ok := valMap[valStr]; ok {
+				// Remove uuid from slice
+				newUuids := []string{}
+				for _, id := range uuids {
+					if id != uuid {
+						newUuids = append(newUuids, id)
+					}
+				}
+				if len(newUuids) == 0 {
+					delete(valMap, valStr)
+				} else {
+					valMap[valStr] = newUuids
+				}
+			}
+			if len(valMap) == 0 {
+				delete(index, k)
+			}
+		}
+	}
+}
+
 func main() {
 	logStore := make(map[string]LogEntry)
+	logOrder := []string{} // maintain insertion order for maxLogs
 	reader := bufio.NewReader(os.Stdin)
 
 	for {
@@ -60,7 +140,20 @@ func main() {
 				Raw:  raw,
 				Flat: flat,
 			}
-			// TODO: Update index and notify websocket clients
+			logOrder = append(logOrder, u)
+			updateIndex(u, flat)
+
+			// Enforce maxLogs
+			if len(logOrder) > maxLogs {
+				oldest := logOrder[0]
+				logOrder = logOrder[1:]
+				if entry, ok := logStore[oldest]; ok {
+					removeFromIndex(oldest, entry.Flat)
+					delete(logStore, oldest)
+					// TODO: Notify websocket clients with update_index
+				}
+			}
+			// TODO: Notify websocket clients with add_logs/update_index
 		}
 		// else: not JSON, just echo
 	}
